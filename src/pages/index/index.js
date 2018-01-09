cff49da7d2b14286366f60e3594fea7357ef01d0
gpyshenko <gpyshenko@gmail.com>
2018-01-09 20:32:56 +0500

    Разбивка конфига на модули и склейка с помощью webpack-merge.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,4 +1,4 @@
 import notification from '../../js/notification';
 notification.welcome("Привет");
 console.log('in index.js');
-alert('reload');
+alert('re');
--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,70 @@ let webpack = require('webpack');
 const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
     source: path.join(__dirname, 'src'),
     dist: path.join(__dirname, 'dist')
 };
 
-module.exports = {
-	entry: {
-		'index': PATHS.source + "/pages/index/index.js",
-        'blog': PATHS.source + "/pages/blog/blog.js"
-	},
-	output: {
-		path: PATHS.dist,
-		filename: "[name].js"
-        //publicPath: "/js"
-    },
-	module: {
-		rules: [
-            {
-                test: /\.pug$/,
-                loader: "pug-loader",
-                options: {
-                    pretty: true
+const common = merge([
+    {
+        entry: {
+            'index': PATHS.source + "/pages/index/index.js",
+            'blog': PATHS.source + "/pages/blog/blog.js"
+        },
+        output: {
+            path: PATHS.dist,
+            filename: "[name].js"
+            //publicPath: "/js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
                 }
-            },
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel-loader"
-			}
-		]
-	},
-    plugins: [
-        new UglifyJsPlugin({
-            parallel: true
-		}),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            chunks: ['index'],
-            template: PATHS.source + '/pages/index/index.pug'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'blog.html',
-            chunks: ['blog'],
-            template: PATHS.source + '/pages/blog/blog.pug'
-        })
-	],
-    devServer: {
-	    hot: true,
-        stats: 'errors-only'
+            ]
+        },
+        plugins: [
+            new UglifyJsPlugin({
+                parallel: true
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                chunks: ['index'],
+                template: PATHS.source + '/pages/index/index.pug'
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'blog.html',
+                chunks: ['blog'],
+                template: PATHS.source + '/pages/blog/blog.pug'
+            })
+        ]
+    },
+    pug()
+]);
+
+module.exports = function (env) {
+    if(env === 'production') {
+        return common
+    }
+    if(env === 'development') {
+        return merge([
+
+            common,
+            devserver()
+        ])
     }
 };
